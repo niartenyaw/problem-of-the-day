6a75d70f655c665721ce37d162d3b0081cae3a96
niartenyaw <amwayne90@gmail.com>
2018-02-28 21:27:10 -0800

    Make k largest n log k

--- k-largest-elements/lib/k_largest_elements.rb
+++ k-largest-elements/lib/k_largest_elements.rb
@@ -1,8 +1,11 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  heap = BinaryMinHeap.new { |a, b| b <=> a }
-  array.each { |el| heap.push(el) }
+  heap = BinaryMinHeap.new { |a, b| a <=> b }
+  array.each do |el|
+    heap.push(el)
+    heap.extract if heap.count > k
+  end
 
   results = []
   k.times { results << heap.extract }
